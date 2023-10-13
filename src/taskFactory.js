870e2d51e4e717356ab3c4faf93193baacef6ccd
Bradley Rispone <bradrispone@gmail.com>
2023-10-13 09:38:47 -0500

    Add formatting for due dates on tasks

--- src/taskFactory.js
+++ src/taskFactory.js
@@ -1,11 +1,15 @@
 // Dates should be added in the format of YYYY-MM-DD
 
+import { format } from 'date-fns';
+
 function createTask (title, desc, dueDate, highPriority) {
     const completed = false;
+    const formattedDueDate = format(dueDate, 'yyyy-MM-dd');
+
     return {
         title,
         desc,
-        dueDate,
+        dueDate: formattedDueDate,
         highPriority,
         completed
     };
