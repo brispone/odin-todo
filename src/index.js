09fa399ff2db9e66cbd9007271c76144e6a3f5bc
Bradley Rispone <bradrispone@gmail.com>
2023-10-16 15:17:21 -0500

    Finish add and edit task logic

--- src/index.js
+++ src/index.js
@@ -19,16 +19,24 @@ const newTaskForm = document.getElementById('new-task-form');
 newTaskForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const projectIndex = newTaskForm.elements['projectId'].value;
+    const taskIndex = newTaskForm.elements['taskId'].value;
     const taskTitle = newTaskForm.elements['title'].value;
     const taskDesc = newTaskForm.elements['description'].value;
     const taskDueDate = new Date(newTaskForm.elements['due-date'].value);
     const taskHighPriority = newTaskForm.elements['priority'].checked;
 
     const newTask = createTask(taskTitle, taskDesc, taskDueDate, taskHighPriority);
-    projectList[projectIndex].taskList.push(newTask);
+
+// If this is a task edit (the taskId will be higher than -1) replace old task with new task from form
+    if(taskIndex > -1) {
+        projectList[projectIndex].taskList[taskIndex] = newTask;
+// Otherwise, push it to the end of the project as a new task
+    } else projectList[projectIndex].taskList.push(newTask);
     
     // Reset the form inputs and hide the new task form after submission
     newTaskForm.reset();
+    newTaskForm.elements['projectId'].value = '123';
+    newTaskForm.elements['taskId'].value = '-1';
     const newTaskDiv = document.getElementById('new-task-form-container');
     newTaskDiv.style.display = 'none';
     
