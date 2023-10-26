f5a442eb2119964407a7c8aeeed3f7dcd88ec965
Bradley Rispone <bradrispone@gmail.com>
2023-10-26 17:07:28 -0500

    Fix bug when no localStorage exists upon initialization

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import { createProject } from './projectFactory';
 import { createTask } from './taskFactory';
 import { renderProjects, renderModals } from './renderFunctions';
 
-let projectList = JSON.parse(localStorage.getItem('projectList'));
-
-if(projectList == null) {
-    projectList = [];
-}
+let projectList = JSON.parse(localStorage.getItem('projectList')) || [];
+localStorage.setItem('projectList', JSON.stringify(projectList));
 
 renderModals();
 renderProjects();
